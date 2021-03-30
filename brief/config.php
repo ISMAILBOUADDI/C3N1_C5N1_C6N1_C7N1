162c23177c7d572913f6118d8decaa1416e52c1d
ismailbouaddi <ismailbouaddi55@gmail.com>
2021-03-30 16:33:58 +0100

    update

--- brief/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-DEFINE('DB_HOST',"localhost");
-DEFINE('DB_USER',"root");
-DEFINE('DB_PASS',"root");
-DEFINE('DB_NAME','web');
-$con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME) 
-or die("Error:can't connect to server");
-?>
